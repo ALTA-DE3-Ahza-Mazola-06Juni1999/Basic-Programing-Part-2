85efbfd97d2313ef9d52f80f6c05f0fc3ce87fb5
Ahza Mazola <ahzamazola@gmail.com>
2024-05-05 16:30:07 +0700

    input perhitungan diskon

--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,9 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+harga = int(input("Masukkan harga "))
+diskon = int(input("Masukkan diskon "))
+
+harga_diskon =(harga * diskon) / 100
+harga_bayar = harga - harga_diskon
+print(harga_bayar)
